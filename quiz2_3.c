a1debff9f1f5a765c94a6e7623351ccc3693ed9e
6320502479 <pattrapol.J@ku.th>
2021-03-25 18:28:10 +0700

    j

---
+++ quiz2_3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+int palin(int a)
+{
+    int i=1,j,k,count=0;
+    while(i!=a)
+    {
+        j=i;
+        k=flip(i);
+        if(j==k)
+        {
+          count++;
+        }
+        i++;
+    }
+    return count;
+}
+int flip(int n)
+{
+    int i=0,n2,j,k=0;
+    n2=n;
+    while(n!=0)
+    {
+        n=n/10;
+        i++;
+    }
+    int a[i],i2=i;
+    for(j=0;j<i;j++)
+    {
+        a[j]=n2%10;
+        n2/=10;
+        k=k+(a[j]*pow(10,i2-1));
+        i2--;
+    }
+    return k;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>=1 && n<=1000000)
+    printf("%d",palin(n));
+}
